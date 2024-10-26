e262b4c6f18dc9f753ed2cddaf7d83c40bc47810
haokai-jojo <haokai@biomap.com>
2024-10-26 16:12:52 +1100

    use res50 mask2former model

--- mask2former.py
+++ mask2former.py
@@ -481,7 +481,8 @@ add_maskformer2_config(cfg)
 
 # Ensure the config file path corresponds to your model's requirements
 cfg.merge_from_file("./Mask2Former/configs/coco/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_100ep.yaml")
-cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/maskformer/mask2former/coco/panoptic/maskformer2_swin_large_IN21k_384_bs16_100ep/model_final_f07440.pkl'
+# cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/maskformer/mask2former/coco/panoptic/maskformer2_swin_large_IN21k_384_bs16_100ep/model_final_f07440.pkl'
+cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/maskformer/mask2former/coco/panoptic/maskformer2_R50_bs16_50ep/model_final_94dc52.pkl'
 cfg.MODEL.MASK_FORMER.TEST.INSTANCE_ON = True
 cfg.MODEL.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
