0b44b5ba8e5d1c69c971f3ca356d58fe6b199450
haokai-jojo <haokai@biomap.com>
2024-10-28 05:59:52 +1100

    best checkpoint trainer develop

--- detectron2/detectron2/engine/trainer.py
+++ detectron2/detectron2/engine/trainer.py
@@ -21,7 +21,7 @@ class CustomTrainer(DefaultTrainer):
     @classmethod
     def test(cls, cfg, model, evaluators=None):
         return super().test(cfg, model, evaluators=COCOEvaluator(
-            cfg.DATASETS.TEST, output_dir=cfg.OUTPUT_DIR
+            cfg.DATASETS.TEST[0], output_dir=cfg.OUTPUT_DIR
         ))
     
 
