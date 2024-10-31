d394f47572025e5611bd215dcbee63b8412b0b7f
haokai-jojo <haokai@biomap.com>
2024-10-31 16:34:01 +1100

    clean up mask_rcnn entry

--- mask_rcnn_train.py
+++ mask_rcnn_train.py
@@ -29,56 +29,58 @@ from detectron2.data import build_detection_test_loader
 from detectron2.data.datasets import register_coco_instances
 from detectron2.data.datasets.turtle_coco import split_n_prepare_turtle_coco, register_turtle_coco
 
-base_dir = "./turtles-data/data"
-# register_coco_instances("turtle_train", {}, os.path.join(base_dir, "annotations_train.json"), "./turtles-data/data")
-# register_coco_instances("turtle_val", {}, os.path.join(base_dir, "annotations_valid.json"), "./turtles-data/data")
-# register_coco_instances("turtle_test", {}, os.path.join(base_dir, "annotations_test.json"), "./turtles-data/data")
+def register_dataset(cfg):
+    base_dir = "./turtles-data/data"
 
-datasets = split_n_prepare_turtle_coco(base_dir, dev_mode=False)
+    datasets = split_n_prepare_turtle_coco(base_dir, dev_mode=False)
 
-for _name, _data in datasets.items():
-    register_turtle_coco(_data, _name, base_dir)
+    for _name, _data in datasets.items():
+        register_turtle_coco(_data, _name, base_dir)
+    
+    cfg.DATASETS.TRAIN = ("turtle_parts_train",)
+    cfg.DATASETS.TEST = ("turtle_parts_valid",)
 
-from detectron2.engine import DefaultTrainer
 
-cfg = get_cfg()
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml"))
-# cfg.merge_from_file(model_zoo.get_config_file("Misc/scratch_mask_rcnn_R_50_FPN_3x_gn.yaml"))
-# cfg.DATASETS.TRAIN = ("turtle_train",)
-# cfg.DATASETS.VALID = ("turtle_val",)
-# cfg.DATASETS.TEST = ("turtle_test",)
+def prepare_model(cfg):
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml"))
 
-# Switch the valid and test split, as the detectron2 uses cfg.DATASETS.TEST for validation
-cfg.DATASETS.TRAIN = ("turtle_parts_train",)
-cfg.DATASETS.TEST = ("turtle_parts_valid",)
+    # Switch the valid and test split, as the detectron2 uses cfg.DATASETS.TEST for validation
+    cfg.DATASETS.TRAIN = ("turtle_parts_train",)
+    cfg.DATASETS.TEST = ("turtle_parts_valid",)
 
-cfg.DATALOADER.NUM_WORKERS = 2
-# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml")
-# cfg.MODEL.WEIGHTS = ""
-cfg.SOLVER.IMS_PER_BATCH = 4  # This is the real "batch size" commonly known to deep learning people
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 7000   # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-cfg.MODEL.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-cfg.TEST.EVAL_PERIOD = 1000
-cfg.INPUT.MASK_FORMAT = 'bitmask'
-# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
-print(cfg)
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml")
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-# trainer = DefaultTrainer(cfg) 
-trainer = CustomTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train()
+    cfg.SOLVER.IMS_PER_BATCH = 4  # This is the real "batch size" commonly known to deep learning people
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 10000   # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.STEPS = []        # do not decay learning rate
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+    cfg.MODEL.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    cfg.TEST.EVAL_PERIOD = 1000
+    cfg.INPUT.MASK_FORMAT = 'bitmask'
+    cfg.OUTPUT_DIR = "./output_mask_rcnn"
+    
+def setup():
+    cfg = get_cfg()
+    register_dataset(cfg)
+    prepare_model(cfg)
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
-cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_best.pth")  # path to the model we just trained
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
-predictor = DefaultPredictor(cfg)
+    return cfg
 
-evaluator = TurtleCOCOEvaluator("turtle_parts_test", output_dir="./output")
-tst_loader = build_detection_test_loader(cfg, "turtle_parts_test")
-print(inference_on_dataset(predictor.model, tst_loader, evaluator))
+if __name__ == '__main__':
+    cfg = setup()
+
+    trainer = CustomTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_best.pth")  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+    predictor = DefaultPredictor(cfg)
+
+    evaluator = TurtleCOCOEvaluator("turtle_parts_test", output_dir="./output")
+    tst_loader = build_detection_test_loader(cfg, "turtle_parts_test")
+    print(inference_on_dataset(predictor.model, tst_loader, evaluator))
