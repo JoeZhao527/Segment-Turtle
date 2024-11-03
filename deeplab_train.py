317b4a4b894fbce83ee487b9f03b71217fc6b241
haokai-jojo <haokai@biomap.com>
2024-11-04 08:27:12 +1100

    deeplab train 20000

--- deeplab_train.py
+++ deeplab_train.py
@@ -57,7 +57,7 @@ def prepare_model(cfg):
 
     cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
     cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.MAX_ITER = 30000   # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.MAX_ITER = 20000   # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
     cfg.SOLVER.STEPS = []        # do not decay learning rate
     
     cfg.MODEL.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
