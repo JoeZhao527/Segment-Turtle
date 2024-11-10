3cd1932d13c14facd4bcfb8b742b161e569e9e1c
haokai-jojo <haokai@biomap.com>
2024-11-10 17:38:42 +1100

    unet large scale inference

--- scripts/focus_unet_evaluate.sh
+++ scripts/focus_unet_evaluate.sh
@@ -1,6 +1,5 @@
 python focused_unet_evaluate.py \
   --background_path=./evaluate_unet/sem_seg_predictions.json \
-  --dev \
   --output_dir=./focused_unet_results/r_01 \
   --focus_crop_ratio=0.1
 
@@ -21,5 +20,15 @@ python focused_unet_evaluate.py \
 
 python focused_unet_evaluate.py \
   --background_path=./evaluate_unet/sem_seg_predictions.json \
-  --output_dir=./focused_unet_results/r_200 \
-  --focus_crop_ratio=20
+  --output_dir=./focused_unet_results/r_20 \
+  --focus_crop_ratio=2.0
+
+python focused_unet_evaluate.py \
+  --background_path=./evaluate_unet/sem_seg_predictions.json \
+  --output_dir=./focused_unet_results/r_50 \
+  --focus_crop_ratio=5.0
+
+python focused_unet_evaluate.py \
+  --background_path=./evaluate_unet/sem_seg_predictions.json \
+  --output_dir=./focused_unet_results/r_100 \
+  --focus_crop_ratio=10.0
