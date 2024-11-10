37dec92879d75e27a808a5953e69a1f266eefca3
haokai-jojo <haokai@biomap.com>
2024-11-10 18:54:27 +1100

    unet large scale inference

--- detectron2/detectron2/data/dataset_mapper.py
+++ detectron2/detectron2/data/dataset_mapper.py
@@ -270,8 +270,8 @@ def focused_cropping(image: np.ndarray, mask: np.ndarray, background_mask: np.nd
     # Find object positions (where mask is 0)
     y_coords, x_coords = np.where(background_mask == 0)
 
-    # Handle case where no foreground pixels are found
-    if len(y_coords) == 0 or len(x_coords) == 0:
+    # Handle case where limited foreground pixels are found
+    if len(y_coords) <= 10 or len(x_coords) <= 10:
         # Return original image and mask without cropping
         original_height = background_mask.shape[0]
         original_width = background_mask.shape[1]
