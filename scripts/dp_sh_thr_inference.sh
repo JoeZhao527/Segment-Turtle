3329b184a1524899423d18167cc6faced78f208c
haokai-jojo <haokai@biomap.com>
2024-11-13 17:41:40 +1100

    prepare dual_prop_rcnn

--- scripts/dp_sh_thr_inference.sh
+++ scripts/dp_sh_thr_inference.sh
@@ -18,9 +18,8 @@ python dual_prop_evaluate.py \
     --score_thresh 0.8 \
     --output_dir evaluate_dual_prop_rcnn
 
-
-# python dual_prop_evaluate.py \
-#   --model_path output_dual_prop_rcnn/model_best.pth \
-#   --score_thresh 0.2 \
-#   --output_dir debug_dual_prop_rcnn \
-#   --dev
+python dual_prop_evaluate.py \
+  --model_path output_dual_prop_rcnn/model_best.pth \
+  --score_thresh 0.2 \
+  --output_dir debug_dual_prop_rcnn \
+  --dev
--- scripts/mask2former_setup.sh
+++
@@ -1,8 +0,0 @@
-# Set up Mask2Former
-CWD=$pwd
-cd Mask2Former/mask2former/modeling/pixel_decoder/ops
-pwd
-sh make.sh
-pip install .
-cd ${CWD}
-pwd
