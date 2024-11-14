ed36c4bac538b94683b99b9d0df3f71da2070db7
haokai-jojo <haokai@biomap.com>
2024-11-14 23:14:05 +1100

    update requirements

--- scripts/deployment_test.sh
+++ scripts/deployment_test.sh
@@ -1,23 +1,23 @@
 python mask_rcnn_train.py \
   --data_dir=./turtles-data/data \
-  --output_dir=./output_mask_rcnn \
+  --output_dir=./output_mask_rcnn_dev \
   --dev
 
 python dual_prop_rcnn.py \
   --data_dir=./turtles-data/data \
-  --output_dir=./output_dual_prop_rcnn \
+  --output_dir=./output_dual_prop_rcnn_dev \
   --score_thresh=0.6 \
   --dev
 
 python unet_train.py \
   --data_dir=./turtles-data/data \
-  --output_dir=./output_unet \
+  --output_dir=./output_unet_dev \
   --dev
 
 python focused_unet_evaluate.py \
   --data_dir=./turtles-data/data \
-  --output_dir=./focused_unet_results \
-  --model_path=./output_unet/model_best.pth \
-  --background_path=./output_unet/sem_seg_predictions.json \
+  --output_dir=./focused_unet_results_dev \
+  --model_path=./output_unet_dev/model_best.pth \
+  --background_path=./output_unet_dev/sem_seg_predictions.json \
   --focus_crop_ratio=0.1 \
   --dev
